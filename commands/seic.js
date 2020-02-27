f90beb3a3c88dcd7cabe5fd50dade11fa43966e9
VictorienXP <VictorienXP@Xperidia.com>
2020-02-27 20:46:24 +0100

    Create seic.js

---
+++ commands/seic.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+require('../discord-patch.js');
+
+module.exports.run = async(bot, msg, args) => {
+
+	bot.channels.get(args[0]).send(`<a:${args[1]}:${args[2]}>`);
+
+}
+
+module.exports.help = {
+	name: "seic"
+}

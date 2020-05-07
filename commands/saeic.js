931192d9e865624afe77901b28e3a3eefcb2f5f4
VictorienXP <VictorienXP@Xperidia.com>
2020-05-07 12:40:16 +0200

    Hey seic not saeic

--- commands/saeic.js
+++ commands/saeic.js
@@ -3,10 +3,10 @@ require('../discord-patch.js');
 
 module.exports.run = async(bot, msg, args) => {
 
-	bot.channels.get(args[0]).send(`<:${args[1]}:${args[2]}>`);
+	bot.channels.get(args[0]).send(`<a:${args[1]}:${args[2]}>`);
 
 }
 
 module.exports.help = {
-	name: "seic"
+	name: "saeic"
 }
